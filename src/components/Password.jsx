363dd08d8fb9f15dad1b410697ff79b488778c9a
Curtis Gibson <notcurtisg@gmail.com>
2021-01-30 17:13:54 -0500

    added Password component

---
+++ src/components/Password.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const $ = require('jquery');
+
+class Password extends React.Component {
+
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<div className="card">
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">Name</li>
+					<li className="list-group-item">here is some stuff</li>
+				</ul>
+			</div>
+		);
+	}
+
+}
+
+export default Password;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
 import './scss/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Password from './components/Password.jsx';
 
 const domContainer = document.querySelector('#react_root');
 
-ReactDOM.render(<p>TODO ajax passwords</p>, domContainer);
-
 // TODO list passwords
+let password = new Password();
+
+ReactDOM.render(password.render(), domContainer);
+
