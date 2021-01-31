375532215e13c3c3e6c22a67d7b3c5dfaf291ac0
Curtis Gibson <notcurtisg@gmail.com>
2021-01-31 13:48:29 -0500

    LoginForm basic conversion complete

--- src/login.jsx
+++ src/login.jsx
@@ -1,19 +1,18 @@
 import './scss/style.scss';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginForm from './components/LoginForm.jsx';
 
 const domContainer = document.querySelector('#react_root');
 
-const loginForm = LoginForm({
-	onToken: token => {
-		const url = new URL(window.location.href);
-		const returnurl = url.searchParams.get('returnurl');
-		if (returnurl) {
-			window.location.href = decodeURIComponent(returnurl);
-		} else {
-			window.location.href = '/';
-		}
+const onToken = token => {
+	const url = new URL(window.location.href);
+	const returnurl = url.searchParams.get('returnurl');
+	if (returnurl) {
+		window.location.href = decodeURIComponent(returnurl);
+	} else {
+		window.location.href = '/';
 	}
-});
+};
 
-ReactDOM.render(loginForm, domContainer);
+ReactDOM.render(<LoginForm onToken={onToken} />, domContainer);
