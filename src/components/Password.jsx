09adc37c021f3e55a1298d8f808bc36bd509fb9e
Curtis Gibson <notcurtisg@gmail.com>
2021-01-30 22:19:04 -0500

    Convert Password to function component

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 const $ = require('jquery');
 
-class Password extends React.Component {
+const Password = (props) => {
+	
+	return (
+		<div className="card">
+			<ul className="list-group list-group-flush">
+				{Object.keys(props).map((propName) => (
+					<li key={propName} className="list-group-item">{propName}: {props[propName]}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
-	constructor() {
-		super();
-	}
-
-	render() {
-		return (
-			<div className="card">
-				<ul className="list-group list-group-flush">
-					<li className="list-group-item">Name</li>
-					<li className="list-group-item">here is some stuff</li>
-				</ul>
-			</div>
-		);
-	}
-
-}
+Password.propTypes = {
+	name: PropTypes.string,
+	username: PropTypes.string,
+	password: PropTypes.string,
+	info: PropTypes.string,
+	pinfo: PropTypes.string
+};
 
 export default Password;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import './scss/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PasswordList from './components/PasswordList.jsx';
+//import PasswordList from './components/PasswordList.jsx';
+import Password from './components/Password.jsx';
 
 const domContainer = document.querySelector('#react_root');
 
 // TODO list passwords
-let passwords = new PasswordList();
+let password = <Password name='a name' username='a username' password='a password' info='some info' pinfo='some pinfo'/>;
 
-ReactDOM.render(passwords.render(), domContainer);
+ReactDOM.render(password, domContainer);
 
