3c21c00009bbc917fc9d64cee59fe88e268fc12a
Curtis Gibson <notcurtisg@gmail.com>
2021-02-01 23:55:46 -0500

    Added legacy clipboard copying

--- src/components/PasswordField.jsx
+++ src/components/PasswordField.jsx
@@ -1,9 +1,21 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const COPY_MESSAGE = 'Copied';
-const COPY_FAIL_MESSAGE = 'Copied';
+const COPY_FAIL_MESSAGE = 'Copy failed';
 const HIDDEN_MESSAGE = '<hidden>';
+
+const copyToClipboardLegacy = (str) => {
+	return new Promise((resolve, reject) => {
+		const el = document.createElement('textarea');
+		el.value = str;
+		document.body.appendChild(el);
+		el.select();
+		const success = document.execCommand('copy');
+		document.body.removeChild(el);
+		if (success) resolve(); else reject();
+	});
+};
 
 const PasswordField = ({ className, label, value, hidden, peekable }) => {
 
@@ -21,26 +33,25 @@ const PasswordField = ({ className, label, value, hidden, peekable }) => {
 		setValue(value);
 	};
 
+	const onSuccess = () => {
+		setValue(COPY_MESSAGE);
+		setTimeout(() => {
+			(isHidden) ? hide() : show();
+		}, 1000);
+	};
+
+	const onFailure = () => {
+		setValue(COPY_FAIL_MESSAGE);
+		setTimeout(() => {
+			(isHidden) ? hide() : show();
+		}, 1000);
+	};
+
 	const copy = () => {
-		if (navigator.clipboard.writeText) {
-			navigator.clipboard.writeText(value).then(() => {
-				console.log('Copied ' + label);
-				setValue(COPY_MESSAGE);
-				setTimeout(() => {
-					(isHidden) ? hide() : show();
-				}, 1000);
-			}).catch(err => {
-				console.log(err);
-				setValue(COPY_FAIL_MESSAGE);
-				setTimeout(() => {
-					(isHidden) ? hide() : show();
-				}, 1000);
-			});
+		if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+			navigator.clipboard.writeText(value).then(onSuccess).catch(onFailure);
 		} else {
-			setValue(COPY_FAIL_MESSAGE);
-			setTimeout(() => {
-				(isHidden) ? hide() : show();
-			}, 1000);
+			copyToClipboardLegacy(value).then(onSuccess).catch(onFailure);
 		}
 	};
 
